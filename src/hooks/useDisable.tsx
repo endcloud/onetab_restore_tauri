2ac234f578098a02d790ea5e298578d98fb64b26
shizi <endcloud@qq.com>
2022-08-21 04:31:32 +0800

    update 1.0.7.

--- src/hooks/useDisable.tsx
+++ src/hooks/useDisable.tsx
@@ -1,19 +1,3 @@
-const disableMenu= () => {
-    if (window.location.hostname === 'tauri.localhost') {
-        return
-    }
-
-    document.addEventListener('contextmenu',  (e: any) => {
-        e.preventDefault()
-        return false
-    },{ capture: true })
-
-    document.addEventListener('selectstart', (e: any) => {
-        e.preventDefault()
-        return false
-    }, { capture: true })
-}
-
 export const useDisable = () => {
     document.addEventListener('contextmenu', event => event.preventDefault());
 }
